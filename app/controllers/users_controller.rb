8d1cc07b77dc81f8be1a73714c27c94cbc2e858b
EC2 Default User <ec2-user@ip-172-31-18-222.ap-northeast-1.compute.internal>
2024-10-18 11:16:27 +0000

    Chuudan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   def index
+    @users = User.all
   end
   
   def show
@@ -9,5 +10,26 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+    @user = User.find(params[:id])
+    
   end
+  
+  def update
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to books_path
+  end
+  
+  private
+def user_params
+  params.require(:user).permit(:name, :profile_image, :introduction)
+end
 end
