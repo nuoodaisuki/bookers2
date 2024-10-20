a2812419ff023aa1526b2e5c88bee8b2aee444f0
EC2 Default User <ec2-user@ip-172-31-18-222.ap-northeast-1.compute.internal>
2024-10-20 11:42:35 +0000

    Chuudan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book)
   end
-  
+
   def index
     @users = User.all
     @user = current_user
     @book = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     end
     @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
     unless user.id == current_user.id
@@ -34,9 +34,10 @@ class UsersController < ApplicationController
     end
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to books_path
+    flash[:notice] = "You have updated user successfully."
+    redirect_to user_path
   end
-  
+
 private
 def user_params
   params.require(:user).permit(:name, :profile_image, :introduction)
